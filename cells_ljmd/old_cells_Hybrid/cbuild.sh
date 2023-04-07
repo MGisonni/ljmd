49be9b7ccdf711269e0971c28efe94a7e1ac01ee
MGisonni <massimo.gisonni@gmail.com>
2023-04-07 13:04:07 +0200

    deleted files

--- cells_ljmd/old_cells_Hybrid/cbuild.sh
+++
@@ -1,9 +0,0 @@
-rm -rf build
-mkdir build
-cd build
-cmake ..
-cmake --build .
-cd examples
-# ./../ljmd-serial.x < argon_108.inp 
-mpirun -np 4 ../ljmd.x 3 < argon_2916.inp 
-mpirun -np 4 ../ljmd.x 3 < argon_78732.inp 
